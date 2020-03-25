f94d65f16b2541f14117d11f2e81282cc0ffe5e8
Bradley Rush <bradleyj.rush@gmail.com>
2020-03-25 17:03:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,7 @@
-name = "bradley"
-language = "Ruby"
+
 def introduction_with_language(name, language)
+  name = "bradley"
+  language = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
